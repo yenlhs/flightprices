e6c0ae3c1b14871d788ad3051ec0dbd6b2117472
Adrian <yenlhs@gmail.com>
2023-10-11 15:41:09 +1100

    added date time picker

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,14 @@
 import { View, Text, TextInput, SafeAreaView, Pressable } from 'react-native';
 import React, { useState } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Feather } from '@expo/vector-icons';
 
 const SearchForm = () => {
 	const [from, setFrom] = useState('');
 	const [to, setTo] = useState('');
+
+	const [departureDate, setDepartureDate] = useState(new Date());
+	const [returnDate, setReturnDate] = useState(new Date());
 
 	const onSearchPress = () => {
 		console.log('Searching...');
@@ -14,6 +19,12 @@ const SearchForm = () => {
 				<Text className='self-center font-semibold'>Search the best prices for you next trip!</Text>
 				<TextInput placeholder='From' value={from} onChangeText={setFrom} className='border border-slate-200 p-2 rounded-md' />
 				<TextInput placeholder='To' value={to} onChangeText={setTo} className='border border-slate-200 p-2 rounded-md' />
+
+				<View className='flex flex-row border border-slate-200 items-center p-2 rounded-md'>
+					<Feather name='calendar' size={24} color='gray' />
+					<DateTimePicker value={departureDate} onChange={(event, date) => setDepartureDate(date)} minimumDate={new Date()} />
+					<DateTimePicker value={returnDate} onChange={(event, date) => setReturnDate(date)} minimumDate={departureDate} />
+				</View>
 				<Pressable className='self-center p-2' onPress={onSearchPress}>
 					<Text>Search</Text>
 				</Pressable>
